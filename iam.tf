19b0ad298f6368db96942d498e71ecea7ee62c81
Carlo Mencarelli <me@carlo.cloud>
2021-12-06 14:14:10 -0700

    Fixes for CF and IAM (#9)
    * Fixes for CF and IAM
    
    * Fix SID

--- iam.tf
+++ iam.tf
@@ -6,9 +6,18 @@ data "aws_iam_policy_document" "github-actions-app-deployment" {
   statement {
     sid = "DeployToS3Object"
     actions = [
-      "s3:PutObject*"
+      "s3:PutObject*",
+      "s3:GetObject*",
+      "s3:DeleteObject*",
     ]
     resources = ["${aws_s3_bucket.artifacts.arn}/*"]
+  }
+  statement {
+    sid = "ListBuckets"
+    actions = [
+      "s3:ListBucket*"
+    ]
+    resources = ["*"]
   }
 }
 
